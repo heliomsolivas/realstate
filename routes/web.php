ee7e29382a5ceb4502e0af5abb9e3c0c40c8c15e
Hélio M S Olivas <heliomsolivas@gmail.com>
2024-01-30 11:36:26 -0300

    Adding new admin pages

--- routes/web.php
+++ routes/web.php
@@ -34,10 +34,12 @@ require __DIR__ . '/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 }); // End Group Admin middleware
 
 Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 }); // End Group Agent middleware
 
-
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
