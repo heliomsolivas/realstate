19c4de218efc1d5e1251dbd6cea13f5c0e78f970
Hélio M S Olivas <heliomsolivas@gmail.com>
2024-01-30 15:20:02 -0300

    Adding data to profile update page

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -56,6 +56,8 @@ class LoginRequest extends FormRequest
             ]);
         }
 
+
+
         Auth::login($user, $this->boolean('remember'));
         RateLimiter::clear($this->throttleKey());
     }
